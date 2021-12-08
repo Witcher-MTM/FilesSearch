d5f53de0187326326b9a75d47f3ae84a0da263f8
Witcher-MTM <phantomit228@gmail.com>
2021-12-08 20:07:38 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OldNameTB = new System.Windows.Forms.TextBox();
+            this.NewNameTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AllFiles = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SwapList = new System.Windows.Forms.Button();
+            this.ResultFindFilesList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // OldNameTB
+            // 
+            this.OldNameTB.Location = new System.Drawing.Point(12, 37);
+            this.OldNameTB.Name = "OldNameTB";
+            this.OldNameTB.Size = new System.Drawing.Size(265, 20);
+            this.OldNameTB.TabIndex = 0;
+            this.OldNameTB.Tag = "";
+            // 
+            // NewNameTB
+            // 
+            this.NewNameTB.Location = new System.Drawing.Point(97, 97);
+            this.NewNameTB.Name = "NewNameTB";
+            this.NewNameTB.Size = new System.Drawing.Size(265, 20);
+            this.NewNameTB.TabIndex = 1;
+            this.NewNameTB.TextChanged += new System.EventHandler(this.NewNameTB_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter old name of file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter new name of file";
+            // 
+            // AllFiles
+            // 
+            this.AllFiles.FormattingEnabled = true;
+            this.AllFiles.Location = new System.Drawing.Point(12, 148);
+            this.AllFiles.Name = "AllFiles";
+            this.AllFiles.Size = new System.Drawing.Size(350, 290);
+            this.AllFiles.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Rename";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SwapList
+            // 
+            this.SwapList.Location = new System.Drawing.Point(287, 119);
+            this.SwapList.Name = "SwapList";
+            this.SwapList.Size = new System.Drawing.Size(75, 23);
+            this.SwapList.TabIndex = 6;
+            this.SwapList.Text = "All files";
+            this.SwapList.UseVisualStyleBackColor = true;
+            this.SwapList.Click += new System.EventHandler(this.SwapList_Click);
+            // 
+            // ResultFindFilesList
+            // 
+            this.ResultFindFilesList.FormattingEnabled = true;
+            this.ResultFindFilesList.Location = new System.Drawing.Point(15, 148);
+            this.ResultFindFilesList.Name = "ResultFindFilesList";
+            this.ResultFindFilesList.Size = new System.Drawing.Size(350, 290);
+            this.ResultFindFilesList.TabIndex = 7;
+            this.ResultFindFilesList.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 450);
+            this.Controls.Add(this.ResultFindFilesList);
+            this.Controls.Add(this.SwapList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AllFiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NewNameTB);
+            this.Controls.Add(this.OldNameTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OldNameTB;
+        private System.Windows.Forms.TextBox NewNameTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox AllFiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button SwapList;
+        private System.Windows.Forms.ListBox ResultFindFilesList;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    enum ListStatus
+    {
+        AllFiles,
+        PatternFiles
+    }
+    public partial class Form1 : Form
+    {
+        ListStatus list;
+        List<string> FilesForRename = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            list = ListStatus.AllFiles;
+        }
+
+        private void NewNameTB_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SwapList_Click(object sender, EventArgs e)
+        {
+            if (list == ListStatus.AllFiles)
+            {
+                this.SwapList.Text = "AllFiles";
+                list = ListStatus.PatternFiles;
+
+                ResultFindFilesList.Visible = false;
+                AllFiles.Visible = true;
+            }
+            else
+            {
+                this.SwapList.Text = "PatternFiles";
+                list = ListStatus.AllFiles;
+
+                AllFiles.Visible = false;
+                ResultFindFilesList.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FilesForRename.Clear();
+            ResultFindFilesList.Items.Clear();
+            AllFiles.Items.Clear();
+
+            Task.Factory.StartNew(() => {
+
+                if (this.OldNameTB.Text != string.Empty && this.NewNameTB.Text != string.Empty)
+                {
+                    if (this.OldNameTB.Text == this.NewNameTB.Text)
+                    {
+                        MessageBox.Show("Good try!");
+                    }
+                    else
+                    {
+                        List<string> files = new List<string>();
+
+                        foreach (var item in Environment.GetLogicalDrives())
+                        {
+                            files = getFilesInDir(item);
+                            if (item.Contains("D"))
+                            {
+                                foreach (var elem in files)
+                                {
+                                    if (Path.GetFileName(elem) == this.OldNameTB.Text)
+                                    {
+                                        FilesForRename.Add(elem);
+                                        ResultFindFilesList.Items.Add(elem);
+                                    }
+                                    else if (Path.GetFileNameWithoutExtension(elem) == this.OldNameTB.Text)
+                                    {
+                                        FilesForRename.Add(elem);
+                                        ResultFindFilesList.Items.Add(elem);
+                                    }
+                                    AllFiles.Items.Add(elem);
+                                }
+                                FoundFiles();
+                            }
+                           
+                        }
+                       
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Fill all fields");
+                }
+
+
+            });
+        }
+        private List<string> getFilesInDir(string dirPath)
+        {
+            List<string> retVal = new List<string>();
+            try
+            {
+                retVal = Directory.GetFiles(dirPath, "*.*", SearchOption.TopDirectoryOnly).ToList();
+                foreach (DirectoryInfo d in new DirectoryInfo(dirPath).GetDirectories("*", SearchOption.TopDirectoryOnly))
+                {
+                    retVal.AddRange(getFilesInDir(d.FullName));
+                }
+            }
+            catch (Exception ex) { }
+            return retVal;
+        }
+        private void Rename()
+        {
+            foreach (var item in FilesForRename)
+            {
+                if (File.Exists($@"{Path.GetDirectoryName(item)}\{this.NewNameTB.Text}"))
+                {
+                    File.Delete($@"{Path.GetDirectoryName(item)}\{this.NewNameTB.Text}");
+                }
+                try
+                {
+                    File.Move(item, $@"{Path.GetDirectoryName(item)}\{this.NewNameTB.Text}{Path.GetExtension(item)}");
+                    MessageBox.Show("File renamed!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void FoundFiles()
+        {
+            if (FilesForRename.Count > 0)
+            {
+                Rename();
+            }
+            else
+                MessageBox.Show("Not found!");
+        }
+    }
+}
